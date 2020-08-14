d23f9ff58cf3dcc8a00072430b4adf76baafcc25
Carolina Reis <carolvitheusmobile@gmail.com>
2020-08-14 17:55:18 -0300

    primeiro commit

--- node/index.ts
+++ node/index.ts
@@ -7,12 +7,16 @@ import {
   method,
 } from '@vtex/api'
 import { Clients } from './clients'
+import { updateLiveUsers } from './event/liveUsersUpdate'
 import { analytics } from './handlers/analytics'
+import { productList } from './resolvers/products'
 
 // Create a LRU memory cache for the Status client.
 // The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
 const memoryCache = new LRUCache<string, any>({ max: 5000 })
 metrics.trackCache('status', memoryCache)
+
+
 
 declare global {
   type Context = ServiceContext<Clients, State>
@@ -22,6 +26,9 @@ declare global {
   }
 }
 
+const TREE_SECONDS_MS = 3 * 1000
+const CONCURRENCY = 10
+
 export default new Service<Clients, State, ParamsContext>({
   clients: {
     implementation: Clients,
@@ -30,11 +37,29 @@ export default new Service<Clients, State, ParamsContext>({
         retries: 2,
         timeout: 10000,
       },
+      events: {
+        exponentialTimeoutCoefficient: 2,
+        exponentialBackoffCoefficient: 2,
+        initialBackoffDelay: 50,
+        retries: 1,
+        timeout: TREE_SECONDS_MS,
+        concurrency: CONCURRENCY,
+      },
     },
   },
   routes: {
     analytics: method({
       GET: [analytics],
     }),
+  },
+  events: {
+    liveUsersUpdate: updateLiveUsers,
+  },
+  graphql: {
+    resolvers: {
+      Query: {
+        productList
+      },
+    },
   },
 })
